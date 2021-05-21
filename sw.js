72ac60d48f7bee507f5f86e235c5717fb4b939af
Rooban <roobank1@gmail.com>
2021-05-21 17:59:06 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
+<script>
 var version="v2.0.2";var swPath;var urlObject=new URL(location);var host;if(urlObject.searchParams.get("swPath")){swPath=urlObject.searchParams.get("swPath");}
 else{if(urlObject.searchParams.get("version")){version=urlObject.searchParams.get("version");}
 if(urlObject.searchParams.get("swJSHost")){host="https://"+urlObject.searchParams.get("swJSHost");}
 else{host="https://sdki.truepush.com/sdk/";}
 swPath=host+version+"/sw.js";}
 importScripts(swPath);
+</script>
