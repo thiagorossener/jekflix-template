009ea0de4fe6c1cf661f4403076e8a871a2bb656
charveey <amlamanangekevin@gmail.com>
2023-05-24 04:29:04 +0000

    fixed anchor generation script

--- src/js/main/anchor.js
+++ src/js/main/anchor.js
@@ -1,14 +1,17 @@
 (function () {
   var headings = document.querySelectorAll('h1[id],h2[id],h3[id],h4[id],h5[id],h6[id]');
   for (var i = 0; i < headings.length; i++) {
-    var img = document.createElement('img');
-    img.setAttribute('src', '/assets/img/link-symbol.svg');
+    // Exclude headings with "welcomments" in their id
+    if (!headings[i].getAttribute('id').includes('welcomments')) {
+      var img = document.createElement('img');
+      img.setAttribute('src', '/assets/img/link-symbol.svg');
 
-    var a = document.createElement('a');
-    a.setAttribute('href', '#' + headings[i].getAttribute('id'));
-    a.classList.add('anchor');
-    a.appendChild(img);
+      var a = document.createElement('a');
+      a.setAttribute('href', '#' + headings[i].getAttribute('id'));
+      a.classList.add('anchor');
+      a.appendChild(img);
 
-    headings[i].insertBefore(a, headings[i].firstChild);
+      headings[i].insertBefore(a, headings[i].firstChild);
+    }
   }
 })();
